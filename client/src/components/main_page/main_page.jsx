7400472dbfb3cbd6686c53f07b80d21e0a56da08
minjeong-kk <minjeongkk0@gmail.com>
2026-04-24 01:13:42 +0900

    프론트 환경 재설정

---
+++ client/src/components/main_page/main_page.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './main_page.css';
+
+function Main() {
+
+  return (
+    <div>
+      <header>
+        <div className="layout">
+            <h1>학습 대시보드</h1>
+            <p>오늘의 과제를 확인하세요!</p>
+        </div>
+        
+      </header>
+
+      <main className="dashboard">
+
+        <div className="left-section">
+          <div className="left">과제탭</div>
+
+          <div className="left">
+            알림탭
+          </div>
+        </div>
+
+        <div className="right">Ai 기능</div>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default Main;
