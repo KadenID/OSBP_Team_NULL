86e3bc3d7e5062c917e26717a923f9560a4a25e5
ya-e-nic <kjh20060829@gmail.com>
2026-04-29 18:31:11 +0900

    security

---
+++ security/password.py
@@ -0,0 +1,12 @@
+import .bcrypt
+
+def hash_password(password):
+    password_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password_bytes, salt)
+    return hashed.decode("utf-8")
+
+def compare_password(password, hashed):
+    password_bytes = password.encode("utd-8")
+    hashed_bytes = hashed.encode("utf-8")
+    return bcrypt.checkpw(password_bytes, hashed_bytes)
