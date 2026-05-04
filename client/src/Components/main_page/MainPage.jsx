26953dcddc420f00be52fa6af9d0894646007521
minjeong-kk <minjeongkk0@gmail.com>
2026-05-04 19:26:17 +0900

    main css 다크 2차 수정 #16

--- client/src/Components/main_page/MainPage.jsx
+++ client/src/Components/main_page/MainPage.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './MainPage.css';
-import AssignmentTab from "../assignment-tab/AssignmentTab.jsx";
+//import AssignmentTab from "../assignment-tab/AssignmentTab.jsx";
+import { FiLogOut, FiHome } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 
 function MainPage() {
 
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
@@ -22,9 +25,17 @@ function MainPage() {
       <header>
           <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <h1>학습 대시보드</h1>
-            <button onClick={toggleTheme} className="theme-toggle-btn">
-                    {isDarkMode ? '🌞' : '🌙'}
-            </button>
+
+              <div className="top-right-menu">
+                  <button onClick={toggleTheme} className="dark-button" style={{ marginRight: '10px' }}>
+                      {isDarkMode ? '🌞  ' : '🌙  '}
+                  </button>
+                  <button type="button" className="my-button" onClick={() => navigate("/mypage")}>
+                      <FiHome className="main-icon" />
+                      Mypage
+                  </button>
+              </div>
+
           </div>
             <p>오늘의 과제를 확인하세요!</p>
         </header>
@@ -32,7 +43,7 @@ function MainPage() {
         <main className="dashboard">
 
           <div className="left-section">
-            <div className="left"><AssignmentTab/></div>
+            {/*<div className="left"><AssignmentTab/></div>*/}
             <div className="left">알림탭</div>
           </div>
 
