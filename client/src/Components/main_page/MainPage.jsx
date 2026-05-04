5fee699be502f3d7910b8919a1688b00e90a80d5
minjeong-kk <minjeongkk0@gmail.com>
2026-05-04 12:35:36 +0900

    mainpage 다크 모드 수정 #16

--- client/src/Components/main_page/MainPage.jsx
+++ client/src/Components/main_page/MainPage.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MainPage.css';
 import AssignmentTab from "../assignment-tab/AssignmentTab.jsx";
 
-
 function MainPage() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
 
   return (
     <>
       <div className="layout">
 
-        <header>
+      <header>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <h1>학습 대시보드</h1>
+            <button onClick={toggleTheme} className="theme-toggle-btn">
+                    {isDarkMode ? '🌞' : '🌙'}
+            </button>
+          </div>
             <p>오늘의 과제를 확인하세요!</p>
         </header>
-
+          
         <main className="dashboard">
 
           <div className="left-section">
