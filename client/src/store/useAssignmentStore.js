75182c1f21f8f091ae76437a56e059380431f787
Kaden <bxj293@naver.com>
2026-05-13 22:30:07 +0900

    store에 동기적 데이터 조작 Action  추가

--- client/src/store/useAssignmentStore.js
+++ client/src/store/useAssignmentStore.js
@@ -3,7 +3,24 @@ import { create } from 'zustand';
 const useAssignmentStore = create((set, get) => ({
   assignment: [],
   isLoading: false,
-  isFetched: false
+  isFetched: false, // 데이터를 이미 불러왔는지 확인하는 플래그
+
+  // 새로운 사용자 과제 추가
+  addAssignment: (newItem) => set((state) => ({
+    assignment: [...state.assignment, newItem]
+  })),
+
+  // 과제 삭제
+  deleteAssignment: (targetId) => set((state) => ({
+    assignment: state.assignment.filter(item => item.id !== targetId)
+  })),
+
+  // 제출 상태 토글
+  toggleSubmit: (id) => set((state) => ({
+    assignment: state.assignment.map(item => 
+      item.id === id ? { ...item, isSubmitted: !item.isSubmitted } : item
+    )
+  }))
 }));
 
 export default useAssignmentStore;
