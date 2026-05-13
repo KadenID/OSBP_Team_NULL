ebadbe629ae00ad0c4c523c891f72ff86335f20f
Kaden <bxj293@naver.com>
2026-05-13 22:36:32 +0900

    store 비동기 함수 뼈대 및 로딩 상태 제어 추가

--- client/src/store/useAssignmentStore.js
+++ client/src/store/useAssignmentStore.js
@@ -5,6 +5,16 @@ const useAssignmentStore = create((set, get) => ({
   isLoading: false,
   isFetched: false, // 데이터를 이미 불러왔는지 확인하는 플래그
 
+  // API 통신으로 과제 불러오기
+  fetchAssignments: async () => {
+    set({ isLoading: true });
+    try {
+      // API 호출 로직이 들어갈 자리
+    } finally {
+      set({ isLoading: false }); // 성공 여부와 관계없이 로딩 종료
+    }
+  },
+
   // 새로운 사용자 과제 추가
   addAssignment: (newItem) => set((state) => ({
     assignment: [...state.assignment, newItem]
