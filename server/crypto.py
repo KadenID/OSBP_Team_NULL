41df1603041a663d7b727e5cb3eb58113a4c5e2d
Kaden <bxj293@naver.com>
2026-05-17 15:01:04 +0900

    AES-256-GCM 암호화 및 Base64 인코딩 로직 구현

--- server/crypto.py
+++ server/crypto.py
@@ -1,4 +1,5 @@
 import os
+import base64
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from dotenv import load_dotenv
 
@@ -17,7 +18,21 @@ if len(SECRET_KEY) != 32:
 aesgcm = AESGCM(SECRET_KEY)
 
 def encrypt(plain_text: str) -> str:
-    pass
+    """
+    평문을 받아 AES-256-GCM으로 암호화하고 base64 문자열로 반환
+    """
+    if not plain_text:
+        return ""
+    
+    # 12바이트 IV(Nonce) 생성
+    iv = os.urandom(12)
+    data = plain_text.encode("utf-8")
+    
+    # 암호화 수행
+    encrypted_data = aesgcm.encrypt(iv, data, None)
+    
+    # IV와 암호화된 데이터를 합쳐서 base64로 인코딩
+    return base64.b64encode(iv + encrypted_data).decode("utf-8")
 
 def decrypt(encrypted_base64: str) -> str:
     pass
