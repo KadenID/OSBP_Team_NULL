15976c4e253f007a12612cf4712442574ddb5681
ybinox <y.bini@chungbuk.ac.kr>
2026-05-20 00:07:39 +0900

    feat: 로그인 액세스 토큰 상태 연결

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import LoginPage from "./Components/login_page/LoginPage";
@@ -6,12 +6,14 @@ import MainPage from "./Components/main_page/MainPage";
 import MyPage from "./Components/my_page/MyPage";
 
 function App() {
+  const [accessToken, setAccessToken] = useState(null);
+  
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/" element={<LoginPage onLogin={setAccessToken} />} />
+        <Route path="/main" element={<MainPage accessToken={accessToken} />} />
+        <Route path="/mypage" element={<MyPage accessToken={accessToken} />} />
       </Routes>
     </Router>
   );
