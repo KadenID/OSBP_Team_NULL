ec1ddbb3315d5d52d396d7be2b4695acdbc13555
Kim junghun <kjh20060829@gmail.com>
2026-05-30 02:42:14 +0900

    security/crypto.py

--- security/crypto.py
+++ security/crypto.py
@@ -1,14 +1,20 @@
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from dotenv import load_dotenv
 import os
 
-key = b'12345678901234567890123456789012'
+load_dotenv()
+
+key = os.getenv("ENCRYPTION_KEY").encode()
 aesgcm = AESGCM(key)
 
 def encrypt(text):
     iv = os.urandom(12)
-    data = text.encode("utf-8")
-    encrypted = aesgcm.encrypt(iv, data, None)
-    return{
+    encrypted = aesgcm.encrypt(
+        iv,
+        text.encode("utf-8"),
+        None
+    )
+    return {
         "iv": iv.hex(),
         "content": encrypted.hex()
     }
@@ -16,7 +22,9 @@ def encrypt(text):
 def decrypt(data):
     iv = bytes.fromhex(data["iv"])
     encrypted = bytes.fromhex(data["content"])
-    decrypted = aesgcm.decrypt(iv, encrypted, None)
-
+    decrypted = aesgcm.decrypt(
+        iv,
+        encrypted,
+        None
+    )
     return decrypted.decode("utf-8")
-
