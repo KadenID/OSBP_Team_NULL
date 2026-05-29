63cb275a08705a89e01bcd692e55c13840b45803
Kim junghun <kjh20060829@gmail.com>
2026-05-30 02:40:45 +0900

    security/storage.py

--- security/storage.py
+++ security/storage.py
@@ -12,3 +12,17 @@ def save_user(student_id, password):
         "student_id": encrypted_id,
         "password": encrypted_pw
     }
+    with open(FILE_NAME, "w") as f:
+        json.dump(data, f, indent=4)
+
+def load_user():
+    try:
+        with open(FILE_NAME, "r") as f:
+            data = json.load(f)
+
+        student_id = decrypt(data["student_id"])
+        password = decrypt(data["password"])
+
+        return student_id, password
+
+
