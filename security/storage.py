a1c9655aea54abc096f82d1155be9aa77939da31
Kim junghun <kjh20060829@gmail.com>
2026-05-30 02:36:19 +0900

    security/storage.py

--- security/storage.py
+++ security/storage.py
@@ -1,7 +1,9 @@
 import json
+import logging
 from security.crypto import encrypt, decrypt
 
 FILE_NAME = "user_data.json"
+logger = logging.getLogger(__name__)
 
 def save_user(student_id, password):
     encrypted_id = encrypt(student_id)
@@ -10,12 +12,3 @@ def save_user(student_id, password):
         "student_id": encrypted_id,
         "password": encrypted_pw
     }
-    with open(FILE_NAME, "w") as f:
-        json.dump(data, f, indent=4)
-
-def load_user():
-    with open(FILE_NAME, "r") as f:
-        data = json.load(f)
-    student_id = decrypt(data["student_id"])
-    password = decrypt(data["password"])
-    return student_id, password
